d70d0697bd966f54d58d70fbae700ff3f3e20067
JoshVComstock <josedanielveizaga@gmail.com>
2024-01-17 22:12:26 -0400

    cambios de ross

---
+++ src/components/inicio/section.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div>
+        <h4>Top Selling</h4>
+        <h2>Top Destination</h2>
+        <div>
+          <div>
+            <img src="" alt="imagen" />
+            <h4>London UK</h4>
+            <h5>10 Days Trip</h5>
+          </div>
+          <div>
+            <img src="" alt="imagen" />
+            <h4>Calculater Weather</h4>
+            <h5>10 Days Trip</h5>
+          </div>
+          <div>
+            <img src="" alt="imagen" />
+            <h4>Full Europe</h4>
+            <h5>10 Days Trip</h5>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Section
