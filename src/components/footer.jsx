507d51bd96d78fcd8475faee7d6744f72fe3360f
JoshVComstock <josedanielveizaga@gmail.com>
2024-01-22 23:34:42 -0400

    navbar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { BodyF, ImgF, Links, ContentF, ContentFI, DivContent } from '../styles/footer';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  BodyF,
+  ImgF,
+  Links,
+  ContentF,
+  ContentFI,
+  DivContent,
+} from "../styles/footer";
 const Footer = () => {
-    return (
-        <BodyF>
-            <ContentFI>
-                <ImgF src="https://www.shutterstock.com/image-photo/portrait-young-female-lawyer-attorney-600nw-2139778293.jpg" alt="imagen" />
-            </ContentFI>
-            <ContentF>
-                <DivContent>
-                    <h4>Company</h4>
-                    <Links to="/nosotros">Nosotros</Links>
-                    <Links to="/areapractica">Áreas de Práctica</Links>
-                </DivContent>
-                <DivContent>
-                    <h4>Contact</h4>
-                    <Links to={`https://api.whatsapp.com/send?phone=${+59170727383}`} target="_blank" rel="noopener noreferrer">WhatsApp 1</Links>
-                    <Links to={`https://api.whatsapp.com/send?phone=${+59170723161}`} target="_blank" rel="noopener noreferrer">WhatsApp 2</Links>
-                </DivContent>
-                <DivContent>
-                    <h4>Social Networks</h4>
-                    <Links to={`https://www.facebook.com/belc.consortium?mibextid=ZbWKwL`}>Facebook</Links>
-                    <Links to={``}>Instagram</Links>
-                    <Links to={``}>TikTok</Links>
-                </DivContent>
-            </ContentF>
-        </BodyF>
-    );
-}
+  return (
+    <BodyF>
+      <ContentFI>
+        <ImgF
+          src="https://www.shutterstock.com/image-photo/portrait-young-female-lawyer-attorney-600nw-2139778293.jpg"
+          alt="imagen"
+        />
+      </ContentFI>
+      <ContentF>
+        <DivContent>
+          <h4>Company</h4>
+          <Links to="/nosotros">Nosotros</Links>
+          <Links to="/areapractica">Áreas de Práctica</Links>
+        </DivContent>
+        <DivContent>
+          <h4>Contact</h4>
+          <Links
+            to={`https://api.whatsapp.com/send?phone=${+59170727383}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WhatsApp 1
+          </Links>
+          <Links
+            to={`https://api.whatsapp.com/send?phone=${+59170723161}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WhatsApp 2
+          </Links>
+        </DivContent>
+        <DivContent>
+          <h4>Social Networks</h4>
+          <Links
+            to={`https://www.facebook.com/belc.consortium?mibextid=ZbWKwL`}
+          >
+            Facebook
+          </Links>
+          <Links to={``}>Instagram</Links>
+        </DivContent>
+      </ContentF>
+    </BodyF>
+  );
+};
 
 export default Footer;
-
--- src/styles/footer.jsx
+++ src/styles/footer.jsx
@@ -4,40 +4,43 @@ import { Colors } from "../styles/colors";
 
 export const BodyF = styled.div`
   width: 100%;
-  height: 100%;
+  height: 220px;
   display: flex;
   flex-direction: row;
   background: #1e1c1c;
 `;
 export const ContentFI = styled.div`
-  width: 50%;
-  height: 50%;
-  flex-direction: row; 
-  margin: 2.5% 2.5%;
+  width: 35%;
+  height:100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding:50px;
 `;
 
 export const ImgF = styled.img`
-  width: 50%; 
-  display: flex;
-  justify-content: center; 
-  align-items: center;
+  width: 200px;
+  height :140px;
+  border-radius: 10px;
 `;
 
 export const ContentF = styled.div`
-  width: 50%;
-  height: 50%;
+  width: 65%;
+  height: 100%;
   display: flex;
   flex-direction: row; 
-  justify-content: center;
-  margin: 2.5% 2%;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
 export const DivContent = styled.div`
   display: flex; 
   flex-direction: column;
-  margin: 0 3%;
+  align-items: center;
+  justify-content: center;
   & > h4{
     color: ${Colors.primary700};
+    font-size: 22px;
   }
 `;
 export const Links = styled(Link)`
